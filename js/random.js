7289d2fd6777f67716bfae97a48a8114a5263ce1
daegons <119998807+daegons@users.noreply.github.com>
2023-03-31 13:39:49 +0900

    Add files via upload

---
+++ js/random.js
@@ -0,0 +1,16 @@
+let imgChange = document.querySelector(".section2 .best > p");
+let mright = document.querySelector(".mright");
+
+imgChange.addEventListener("click", () => {
+  const imgArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const randomValue = imgArray[Math.floor(Math.random() * imgArray.length)];
+
+  let change = "";
+
+  change += `<img src="./img/${randomValue}.jpg" alt="#" /><img src="./img/${
+    randomValue + 1
+  }.jpg" alt="#" /><img src="./img/${randomValue - 1}.jpg" alt="#"/>`;
+  
+  $(".mright>img").remove();
+  $(".mright").append(change);
+});
