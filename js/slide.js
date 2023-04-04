29de4293469e2b531548e81250f4404d7aecbb20
daegons <119998807+daegons@users.noreply.github.com>
2023-04-04 16:45:04 +0900

    Add files via upload

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,7 @@
 let imgBox = document.querySelector(".imgBox");
 let imgBoxAll = document.querySelectorAll(".imgBox");
 const imgWrap = document.querySelectorAll(".img1");
+const article1 = document.querySelector('.article1')
 let cloneFirst = imgWrap[0].cloneNode(true); // 요소 복사
 let cloneLast = imgWrap[imgWrap.length - 1].cloneNode(true);
 imgBox.insertBefore(cloneLast, imgWrap[0]);
@@ -10,7 +11,10 @@ let btn = document.querySelector(".img1"); // 버튼 클릭 함수 실행을 위
 let btnAll = document.querySelectorAll(".img1"); // 버튼 클릭 함수 실행을 위한 변수
 let count = 0;
 
-if (window.innerWidth > 1100) {
+
+
+
+if (window.innerWidth > 625) {
   document.querySelector(".imgBox").style.left = "-450px";
 
   function imgSlideShow() {
@@ -35,13 +39,11 @@ if (window.innerWidth > 1100) {
     imgBox.style.transition = `all 1s`;
     if (count > 0) {
       count--;
-      document.querySelector(".imgBox").style.transform = `translate(${
-        count * -450
-      }px)`;
+      document.querySelector(".imgBox").style.transform = `translate(${count * -450
+        }px)`;
       btnAll.forEach((i) => (i.style.background = "rgb(255, 255, 255, .5)"));
       btnAll[count].style.background = "rgb(255, 255, 255, 1)";
     } else if (count === 0) {
-      console.log("asd");
       imgBox.style.transform = `translate(450px)`;
       setTimeout(() => {
         imgBox.style.transition = `0s`;
@@ -55,9 +57,8 @@ if (window.innerWidth > 1100) {
     imgBox.style.transition = `all 1s`;
     if (count < imgWrap.length - 1) {
       count++;
-      document.querySelector(".imgBox").style.transform = `translate(${
-        count * -450
-      }px)`;
+      document.querySelector(".imgBox").style.transform = `translate(${count * -450
+        }px)`;
       btnAll.forEach((i) => (i.style.background = "rgb(255, 255, 255, .5)"));
       btnAll[count].style.background = "rgb(255, 255, 255, 1)";
     } else if (count === imgWrap.length - 1) {
@@ -96,13 +97,11 @@ if (window.innerWidth <= 625) {
     imgBox.style.transition = `all 1s`;
     if (count > 0) {
       count--;
-      document.querySelector(".imgBox").style.transform = `translate(${
-        count * -280
-      }px)`;
+      document.querySelector(".imgBox").style.transform = `translate(${count * -280
+        }px)`;
       btnAll.forEach((i) => (i.style.background = "rgb(255, 255, 255, .5)"));
       btnAll[count].style.background = "rgb(255, 255, 255, 1)";
     } else if (count === 0) {
-      console.log("asd");
       imgBox.style.transform = `translate(280)`;
       setTimeout(() => {
         imgBox.style.transition = `0s`;
@@ -116,9 +115,8 @@ if (window.innerWidth <= 625) {
     imgBox.style.transition = `all 1s`;
     if (count < imgWrap.length - 1) {
       count++;
-      document.querySelector(".imgBox").style.transform = `translate(${
-        count * -280
-      }px)`;
+      document.querySelector(".imgBox").style.transform = `translate(${count * -280
+        }px)`;
       btnAll.forEach((i) => (i.style.background = "rgb(255, 255, 255, .5)"));
       btnAll[count].style.background = "rgb(255, 255, 255, 1)";
     } else if (count === imgWrap.length - 1) {
@@ -175,42 +173,95 @@ function stopSlide() {
   }
 }
 
+
+
 // 스크롤 슬라이드
 if (window.innerWidth > 1100) {
   const mainBtn = document.querySelectorAll(".mainBtn > li");
   const target = document.querySelector(".container");
   const target_top = target.getBoundingClientRect().height;
+  mainBtn[0].style.background = "rgb(191, 7, 255, 1)";
+
 
   let forSlide = 1;
   mainBtn.forEach((i) => {
-    i.addEventListener("click", (e) => {
+    i.addEventListener("click", () => {
       const num = i.value;
       forSlide = num;
       mHtml.animate({ scrollTop: (num - 1) * target_top });
       mainBtn.forEach((i) => (i.style.background = "rgb(191, 7, 255, .3)"));
       mainBtn[forSlide - 1].style.background = "rgb(191, 7, 255, 1)";
+      if (forSlide == 2) {
+        article1.classList.add('on')
+      } else {
+        article1.classList.remove('on')
+      }
     });
   });
-  mainBtn[0].style.background = "rgb(191, 7, 255, 1)";
+
   // 스크롤이벤트
-  for (let i in imgBoxAll) var mHtml = $("html");
+  // for (let i in imgBoxAll) 
+  var mHtml = $("html");
   // 새로고침시 원래자리로 돌아옴
   mHtml.animate({ scrollTop: 0 }, 500);
 
+
   $(window).on("wheel", function (e) {
-    if (mHtml.is(":animated")) return;
-    if (e.originalEvent.deltaY > 0) {
-      if (forSlide == 2) return;
-      mainBtn.forEach((i) => (i.style.background = "rgb(191, 7, 255, .3)"));
-      mainBtn[forSlide].style.background = "rgb(191, 7, 255, 1)";
-      forSlide++;
-    } else if (e.originalEvent.deltaY < 0) {
-      if (forSlide == 1) return;
-      mainBtn.forEach((i) => (i.style.background = "rgb(191, 7, 255, .3)"));
-      mainBtn[forSlide - 2].style.background = "rgb(191, 7, 255, 1)";
-      forSlide--;
+    if (e.originalEvent.deltaY != 0) {
+      // if (mHtml.is(":animated"));
+      if (e.originalEvent.deltaY > 0) {
+        if (forSlide == 2) return;
+        mainBtn.forEach((i) => (i.style.background = "rgb(191, 7, 255, .3)"));
+        mainBtn[forSlide].style.background = "rgb(191, 7, 255, 1)";
+        article1.classList.add('on')
+        forSlide++;
+      } else if (e.originalEvent.deltaY < 0) {
+        if (forSlide == 1) return;
+        mainBtn.forEach((i) => (i.style.background = "rgb(191, 7, 255, .3)"));
+        mainBtn[forSlide - 2].style.background = "rgb(191, 7, 255, 1)";
+        article1.classList.remove('on')
+        forSlide--;
+      }
+      var posTop = (forSlide - 1) * target_top;
+      mHtml.animate({ scrollTop: posTop });
     }
-    var posTop = (forSlide - 1) * target_top;
-    mHtml.animate({ scrollTop: posTop });
-  });
+  })
+
+  $('#map').on("wheel", function (e) {
+    e.stopPropagation()
+  })
+
+
+
+  let target2 = document.querySelector(".section2");
+  let targetTop = target2.getBoundingClientRect().top + target2.clientHeight / 5;
+
+  // $(window).on("scroll", function () {
+  //   let sct = $(this).scrollTop() + $(this).height();
+  //   if (sct >= targetTop) {
+  //     mainBtn.forEach((i) => (i.style.background = "rgb(191, 7, 255, .3)"));
+  //     mainBtn[1].style.background = "rgb(191, 7, 255, 1)";
+  //     article1.classList.add('on')
+  //   } else {
+  //     mainBtn.forEach((i) => (i.style.background = "rgb(191, 7, 255, .3)"));
+  //     mainBtn[0].style.background = "rgb(191, 7, 255, 1)";
+  //     article1.classList.remove('on')
+  //   }
+  // })
 }
+
+
+if (window.innerWidth <= 1100) {
+  article1.classList.remove('on')
+
+  // $(window).on("scroll", function (e) {
+  //   let sct = $(this).scrollTop() + $(this).height();
+  //   if (sct > targetTop) {
+  //     article1.classList.add('on')
+  //   } else {
+  //     article1.classList.remove('on')
+  //   }
+  // })
+}
+
+
